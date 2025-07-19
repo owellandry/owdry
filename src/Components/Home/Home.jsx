94cdc92196b8e1711d8322ef6b6185fa4cd131d3
owell andry <owellandry@cubysoft.uk>
2025-07-19 11:03:31 -0500

    Hello World animado y bonito

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,55 @@
 import React from "react";
+import "./Home.css";
 
-const Inicio = () => {
+const Home = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="home-container">
+      <div className="background-animation">
+        <div className="floating-circle circle-1"></div>
+        <div className="floating-circle circle-2"></div>
+        <div className="floating-circle circle-3"></div>
+        <div className="floating-circle circle-4"></div>
+      </div>
+      
+      <div className="content">
+        <div className="hello-wrapper">
+          <h1 className="hello-text">
+            <span className="word">Hello</span>
+            <span className="word">World</span>
+          </h1>
+          <div className="subtitle">
+            <span className="letter">B</span>
+            <span className="letter">i</span>
+            <span className="letter">e</span>
+            <span className="letter">n</span>
+            <span className="letter">v</span>
+            <span className="letter">e</span>
+            <span className="letter">n</span>
+            <span className="letter">i</span>
+            <span className="letter">d</span>
+            <span className="letter">o</span>
+            <span className="space"></span>
+            <span className="letter">a</span>
+            <span className="space"></span>
+            <span className="letter">m</span>
+            <span className="letter">i</span>
+            <span className="space"></span>
+            <span className="letter">m</span>
+            <span className="letter">u</span>
+            <span className="letter">n</span>
+            <span className="letter">d</span>
+            <span className="letter">o</span>
+          </div>
+        </div>
+        
+        <div className="pulse-button">
+          <button className="cta-button">
+            Explorar
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Inicio;
+export default Home;
